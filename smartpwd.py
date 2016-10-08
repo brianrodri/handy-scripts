e9cf454bf86a758fc0230a9a83f2959c6e825fa0
Brian Rodriguez <thatbrod@gmail.com>
2016-10-08 03:02:06 -0400

    Improved readability.

--- smartpwd.py
+++ smartpwd.py
@@ -10,7 +10,6 @@ import sys
 def smartpwd(pwd, home, maxlen):
   """Modify pwd to show as much useful information as possible within maxlen."""
   SEPERATOR = ".../"
-  
   if pwd.startswith(home):
     pwd = "~" + pwd[len(home):]
   if len(pwd) > maxlen:
@@ -19,19 +18,17 @@ def smartpwd(pwd, home, maxlen):
     pwd = SEPERATOR.join([head[:available_space], tail])
   return pwd
 
-def main():
-  cwd = os.getcwd()
-  home = os.getenv("HOME", "")
 
+def main():
+  cwd, home = os.getcwd(), os.getenv("HOME", "")
   try:
     maxlen = int(os.getenv("PROMPT_LENGTH"))
   except TypeError:  # $PROMPT_LENGTH did not exist.
     maxlen = 40
   except ValueError:
-    maxlen = 40
-    print("$PROMPT_LENGTH exists, but isn't an int.", file=sys.stderr)
-
-  print(smartpwd(cwd, home, maxlen), end="")
+    maxlen = 40  # $PROMPT_LENGTH exists, but isn't an int.
+  finally:
+    print(smartpwd(cwd, home, maxlen), end="")
 
 
 if __name__ == "__main__":
