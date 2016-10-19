c92d2dce2a77a2653c5578920958c22b37df210b
Brian Rodriguez <thatbrod@gmail.com>
2016-10-19 09:38:38 -0400

    Update secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 import random
 import smtplib
 import sys
@@ -9,51 +9,57 @@ from getpass import getpass
 from itertools import combinations, groupby, product
 
 
-def BijectionsOf(domain, codomain, relations):
+def BijectionsOf(domain, codomain=None, relations=None):
   """Return all subsets of relations which create 1-to-1 mappings."""
+  if codomain is None:
+    codomain = domain
+  if relations is None:
+    relations = set(product(domain, codomain))
+    if codomain is domain:
+      relations -= set(zip(domain, domain))
+  elif not all(x in domain and y in codomain for x, y in relations):
+    raise ValueError("relations contains an undefined pair.")
+
   mapping = defaultdict(set)
   for x, y in relations:
     mapping[x].add(y)
-  subsets = map(list, product(*mapping.values()))
-  spanning_subsets = filter(lambda ys: len(set(ys)) == len(domain), subsets)
-  return [tuple(zip(mapping.keys(), ys)) for ys in spanning_subsets]
-
-
-Participant = namedtuple("Participant", "name, email")
+  for ys in product(*mapping.values()):
+    if len(set(ys)) == len(domain):
+      yield tuple(zip(mapping.keys()))
 
 
 def main():
   # Fetch input.
+  Participant = namedtuple("Participant", "name, email")
   members = set()
-  print("Enter one participant per line in the format: \"name, e-mail\"")
+  print("Enter one participant per line w/ the format: \"name, e-mail\"")
   for line in sys.stdin.readlines():
     data = line.split(",", 1)
     name, email = data[0].strip(), data[1].strip()
     members.add(Participant(name, email))
 
   # Choose a random group of members.
-  pairings = set(product(members, members)) - set(zip(members, members))
   try:
-    group = random.choice(BijectionsOf(members, members, pairings))
+    group = random.choice(BijectionsOf(members))
   except ValueError:
     print("No matches.", file=sys.stderr)
     sys.exit()
 
   # Send out the e-mails.
+  subject = "Secret Santa 2016!"
+  body = "Hey {} you got {}, and the spending limit this year is $40!"
   messenger, password = input("E-Mail: "), getpass()
   with smtplib.SMTP("smtp.gmail.com:587") as server:
     server.ehlo()
     server.starttls()
     server.login(messenger, password)
-    subject = "Secret Santa 2016!"
-    body = "Hey {} you got {}, and the spending limit this year is $40!"
     for santa, santee in group:
       msg = MIMEText(body.format(santa.name, santee.name))
       msg["Subject"] = subject
       msg["From"] = messenger
       msg["To"] = santa.email
       server.send_message(msg)
-    print("Done.")
+  print("Done.")
 
 
 if __name__ == "__main__":
