330dc45b277c1b70144cf303755b91a097ce2d16
Brian Rodriguez <thatbrod@gmail.com>
2018-09-15 22:37:45 -0400

    Update secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -35,7 +35,7 @@ def MakeRandomPairings(items):
 
 
 EMAIL_SUBJECT = 'Secret Santa 2018!'
-EMAIL_BODY = 'yo %s, you got %s. spending limit this year is: ??$'
+EMAIL_BODY = 'yo %s, you got %s'
 
 Player = collections.namedtuple('Player', 'name, email')
 PLAYERS = [
