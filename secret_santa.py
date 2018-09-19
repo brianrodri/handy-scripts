f64738115e2f9127dd8328f910357a3b1c86b348
Brian Rodriguez <thatbrod@gmail.com>
2018-09-18 23:48:53 -0400

    Update secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -16,12 +16,16 @@ import sys
 def MakeRandomPairings(items):
     """Return random list of pairs such that items appear once on each side.
 
-    Items are never paired with themselves, unless only one item is present.
+    This function returns a bijection on items to itself. Additionally, the
+    bijection is guaranteed to never pair an item to itself (unless just one
+    item is present).
 
     Example:
         >>> MakeRandomPairings([1, 2, 3])
         ... [(2, 1), (1, 3), (3, 2)]  # Possible outcome
         >>> [(3, 1), (1, 3), (2, 2)]  # Impossible outcome
+        >>> MakeRandomPairings([1])
+        ... [(1, 1)]  # Only outcome
     Args:
         items: iterable of anything.
     Returns:
@@ -37,19 +41,18 @@ def MakeRandomPairings(items):
 
 Player = namedtuple('Player', 'name, email')
 players = [
-    Player(name='Brian', email='thatbrod@gmail.com'),
-    Player(name='Rianb', email='dthatbro@gmail.com'),
-    Player(name='Ianbr', email='odthatbr@gmail.com'),
+    Player(name='test', 'anon@anon.com'),
+    Player(name='me', 'thatbrod@gmail.com'),
 ]
 if '--send' in sys.argv[1:]:
     with smtplib.SMTP('smtp.gmail.com:587') as server:
         server.ehlo(), server.starttls()  # Low-level connection stuff.
-        email_sender = input('Gmail address: ')
-        server.login(email_sender, getpass())
+        sending_address = input('Gmail address: ')
+        server.login(sending_address, getpass())
         for santa, santee in MakeRandomPairings(players):
-            email = MIMEText(f'yo {santa.name}, you got {santee.name}.')
+            email = MIMEText(f'yo {santa.name}, you got {santee.name}')
             email['Subject'] = 'Secret Santa 2018!'
-            email['From'] = email_sender
+            email['From'] = sending_address
             email['To'] = santa.email
             server.send_message(email)
         print('Sent')
