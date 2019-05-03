5aa12297757ae7bb23e991ae2f1d20d7c5db1fc7
Brian Rodriguez <thatbrod@gmail.com>
2019-05-03 13:16:53 -0400

    Update secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -1,58 +1,52 @@
 #!/usr/bin/env python3
-"""Provides a "truly random" set of pairings for secret santa players.
-
-Run this script with `--send` to send emails about designations to each player.
-"""
+from dataclasses import dataclass
 from email.mime.text import MIMEText
 from getpass import getpass
+from itertools import cycle
+from random import shuffle
+from smtplib import SMTP
 from typing import Any, Iterable, List, Tuple
-
-import collections
-import itertools
-import random
-import smtplib
 import sys
 
 
-def MakeRandomPairings(items: Iterable[Any]) -> List[Tuple[Any, Any]]:
-    """Returns a list of random pairs which form a bijection on items to itself.
+@dataclass
+class Player():
+    name: str
+    email: str
 
-    The bijection is guaranteed to contain *at most* 1 cycle.
 
-    Example:
-        >>> MakeRandomPairings([1, 2, 3, 4])
-        ... [(2, 1), (1, 3), (3, 4), (4, 2)]  # Possible outcome
-        >>> [(3, 2), (2, 3), (1, 4), (4, 1)]  # Impossible outcome
-    Args:
-        items: must be finite.
-    """
-    items = [i for i in items]  # Use an independent list of items.
-    if items:
-        random.shuffle(items)
-        items_iter = iter(items)
-        offset_items_iter = itertools.cycle(items)
-        _ = next(offset_items_iter)  # Drop first item to offset it from items_iter.
-        return list(zip(items_iter, offset_items_iter))
-    else:
+def MakeRandomPairs(players):
+    """Returns list of random pairs with exactly one cycle."""
+    players = list(players)
+    if not players:
         return []
+    shuffle(players)
+    rotated_players = cycle(players)
+    next(rotated_players)  # Rotate by 1.
+    return list(zip(players, rotated_players))
 
 
-Player = collections.namedtuple('Player', 'name, email')
-players = [
-    Player(name="Brian", email="thatbrod@gmail.com"),  # Change this (please) :)
-]
+pairs = MakeRandomPairs([
+    Player(name="Me", email="me@gmail.com"),
+    Player(name="Her", email="her@gmail.com"),
+    Player(name="Him", email="him@gmail.com"),
+    Player(name="Them", email="them@gmail.com"),
+    Player(name="Us", email="us@gmail.com"),
+])
+
 if '--send' in sys.argv[1:]:
-    with smtplib.SMTP('smtp.gmail.com:587') as server:
-        server.ehlo(), server.starttls()  # Low-level connection stuff.
-        sending_address = input('Gmail address: ')
-        server.login(sending_address, getpass())
-        for santa, santee in MakeRandomPairings(players):
-            email = MIMEText(f'yo {santa.name}, you got {santee.name}')
-            email['Subject'] = 'Secret Santa 2018!'
-            email['From'] = sending_address
+    with SMTP('http://smtp.gmail.com:587') as server:
+        server.ehlo(), server.starttls()
+        mock_sender = input('Gmail address: ')
+        server.login(mock_sender, getpass())
+        for santa, santee in pairs:
+            email = MIMEText(
+                f'Yo {santa.name}, you got {santee.name}!')
+            email['Subject'] = 'Secret Santa!'
+            email['From'] = mock_sender
             email['To'] = santa.email
             server.send_message(email)
-        print('Sent')
+        print('Sent!')
 else:
-    for santa, santee in MakeRandomPairings(players):
+    for santa, santee in pairs:
         print(f'{santa.name} -> {santee.name}')
