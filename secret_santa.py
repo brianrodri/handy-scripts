d3ea8c7f0b5e09541a5fe3ada54f2218b1b1337a
Brian Rodriguez <brian@brianrodri.com>
2019-11-04 21:42:00 -0500

    improved naming

--- secret_santa.py
+++ secret_santa.py
@@ -18,18 +18,18 @@ def MakeRandomPairs(items):
 
 
 @dataclasses.dataclass(frozen=True)
-class Player():
+class Participant():
     name: str
     email_address: str
 
 
 def main():
-    player_pairs = MakeRandomPairs([
-        Player(name="Me", email_address="me@gmail.com"),
-        Player(name="Her", email_address="her@gmail.com"),
-        Player(name="Him", email_address="him@gmail.com"),
-        Player(name="Them", email_address="them@gmail.com"),
-        Player(name="Us", email_address="us@gmail.com"),
+    matches = MakeRandomPairs([
+        Participant(name='Me', email_address='me@gmail.com'),
+        Participant(name='Her', email_address='her@gmail.com'),
+        Participant(name='Him', email_address='him@gmail.com'),
+        Participant(name='Them', email_address='them@gmail.com'),
+        Participant(name='Us', email_address='us@gmail.com'),
     ])
 
     if '--send' in sys.argv[1:]:
@@ -37,16 +37,16 @@ def main():
             server.starttls()  # Gmail requires TLS encryption.
             sender_gmail_address = input('Gmail address: ')
             server.login(sender_gmail_address, getpass.getpass())
-            for santa, santee in player_pairs:
+            for santa, santee in matches:
                 message = email.message.EmailMessage()
                 message.set_content(f'Yo {santa.name}, you got {santee.name}!')
-                message['Subject'] = 'Secret Santa!'
-                message['From'] = sender_gmail_address
-                message['To'] = santa.email_address
+                message['subject'] = 'Secret Santa!'
+                message['from'] = sender_gmail_address
+                message['to'] = santa.email_address
                 server.send_message(message)
-        print('Sent!')
+        print('Done!')
     else:
-        for santa, santee in player_pairs:
+        for santa, santee in matches:
             print(f'{santa.name} -> {santee.name}')
 
 
